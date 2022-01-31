07f0edcaa5fac0c64b91e3f7feaf421e2ad55fb7
Md. Hasanur Rahman Riad <57297996+HR-Riad@users.noreply.github.com>
2022-01-31 16:08:47 +0600

    Add files via upload

---
+++ multipleperameter.js
@@ -0,0 +1,17 @@
+// function addTwoNum(num1,num2)
+// {
+//     console.log(num1,num2);
+
+// }
+// addTwoNum(12,22);
+
+function addTwoNumbers(num1,num2)
+{
+    console.log(num1,num2);
+    var total= num1 + num2;
+    return total;
+}
+var firstNum=10;
+var secondNum=33;
+result=addTwoNumbers(firstNum,secondNum);
+console.log('Toatal result',result);
