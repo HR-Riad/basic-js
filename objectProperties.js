5f82baef28dde3d0a4b78812c90b7485685f0128
Md. Hasanur Rahman Riad <57297996+HR-Riad@users.noreply.github.com>
2022-01-31 17:19:44 +0600

    Add files via upload

---
+++ objectProperties.js
@@ -0,0 +1,39 @@
+var mobile ={
+    name:'one plus',
+    color:'blue',
+    display:'curve',
+    price:46200,
+    storage:'256gb'
+}
+// console.log(mobile);
+// console.log(mobile.color);
+var mobilePrice=mobile.price;
+
+// console.log(mobilePrice);
+// mobile.price=30000;
+// console.log(mobile);
+
+// mobile["price"]=27900;
+// console.log(mobile);
+
+var  priceMobile="price";
+
+mobile.price=30000;
+
+mobile["price"]=27900;
+
+mobile[priceMobile]= 25000;
+
+// console.log(mobile);
+
+// var storageProperty= 'storage';
+// mobile[storageProperty]='512gb';
+
+// var storageProperty= 'storage';
+// mobile[storageProperty]='512gb';
+// mobile['storage']='1tb';
+
+mobile.storage='2tb';
+
+
+console.log(mobile);
