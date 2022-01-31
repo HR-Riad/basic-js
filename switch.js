68ec24fdfa51e66a575345f7b341e28b65389383
Md. Hasanur Rahman Riad <57297996+HR-Riad@users.noreply.github.com>
2022-01-31 17:58:08 +0600

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,46 @@
+var color='yellow';
+if (color=='red'){
+    console.log('color is red');
+}
+else if (color=='blue'){
+    console.log('color is blue');
+}
+else if (color=='white')
+{
+    console.log('color is white');
+}
+else if (color=='purple'){
+    console.log('color is purple');
+}
+else if (color=='yellow'){
+    console.log('color is yellow');
+}
+else{
+    console.log('color is black');
+}
+
+
+// same solve in switch
+switch (color){
+    case 'red':
+    console.log('color is red');
+    break;
+    case 'blue':
+    console.log('color is blue');
+    break;
+    case 'white':
+    console.log('color is white');
+    break;
+    case 'purple':
+    console.log('color is purple');
+    break;
+    case 'yellow':
+    console.log('color is yellow');
+    break;
+
+    default:
+        console.log('color is black');
+
+    }
+
+
